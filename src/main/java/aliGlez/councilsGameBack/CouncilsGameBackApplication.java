e30099e0c07bf00e353bafc89d2e5ab96e76166b
Alicia González Castro <aliglezcastro@gmail.com>
2024-03-11 20:13:55 +0100

    first commit

---
+++ src/main/java/aliGlez/councilsGameBack/CouncilsGameBackApplication.java
@@ -0,0 +1,13 @@
+package aliGlez.councilsGameBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouncilsGameBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouncilsGameBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aliGlez/councilsGameBack/CouncilsGameBackApplicationTests.java
@@ -0,0 +1,13 @@
+package aliGlez.councilsGameBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouncilsGameBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
